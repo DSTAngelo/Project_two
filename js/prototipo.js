c4e1ada906d7889dcd5d3c0172128c48defdfb7b
DSTAngelo <dstgps01@gmail.com>
2022-05-23 13:02:19 -0500

    A3-23052022

--- js/prototipo.js
+++ js/prototipo.js
@@ -1,12 +1,71 @@
 
-function cargaDePalabras(x) {
+function letrasValido(e) {
+    key = e.keyCode || e.which;
+    tecla = String.fromCharCode(key).toLowerCase();
+    letras = " abcdefghijklmnñopqrstuvwxyz";
+    especiales = [8, 37, 39, 46];
 
-    const listado = ['casa', 'perro', 'gato','elefante','gallina','vaca','loro'];
+    tecla_especial = false
+    for(var i in especiales) {
+        if(key == especiales[i]) {
+            tecla_especial = true;
+            break;
+        }
+    }
+
+    if(letras.indexOf(tecla) == -1 && !tecla_especial)
+        return false;
+}
+
+
+
+function minuscula() {
+    var x=document.getElementById("caracter").value;
+    document.getElementById("caracter").value=x.toLowerCase();
+ }
+
+
+function limpiarCaja() {
+    var content;
+    var focusA;
+    content = document.getElementById('caracter').value = "";
+    focusA = document.getElementById('caracter').focus();
+ }
+
+function cargaDePalabras(x) {
+    const listado = ['casa', 'perro', 'gato','elefante','gallina','vaca','loro','caballo','serpiente','conejo'];
     palabra_Buscar = listado[Math.floor(Math.random()*listado.length)];
     console.log(listado);
     console.log(palabra_Buscar);
     return x;
 }
+
+function cargaPlantas(x) {
+    const listado = ['menta', 'helecho', 'pino','eucalipto','trebol','aguacate','pasto','cebollin','tomate','pepino'];
+    palabra_Buscar = listado[Math.floor(Math.random()*listado.length)];
+    console.log(listado);
+    console.log(palabra_Buscar);
+    return x;
+}
+
+function cargaCosas(x) {
+    const listado = ['casa', 'perro', 'gato','elefante','gallina','vaca','loro','caballo','serpiente','conejo'];
+    palabra_Buscar = listado[Math.floor(Math.random()*listado.length)];
+    console.log(listado);
+    console.log(palabra_Buscar);
+    return x;
+}
+
+function cargaPelis(x) {
+    const listado = ['casa', 'perro', 'gato','elefante','gallina','vaca','loro','caballo','serpiente','conejo'];
+    palabra_Buscar = listado[Math.floor(Math.random()*listado.length)];
+    console.log(listado);
+    console.log(palabra_Buscar);
+    return x;
+}
+
+
+
 
 
 
@@ -18,39 +77,37 @@ function textoConvertir() {
 
 function evaluacion() {
     textoConvertir();
+    limpiarCaja();
     let oculto = palabra_Buscar.replace(/./g, "_ ");
     let contadorErrores = 0;
     document.querySelector('#evaluar').addEventListener('click', () =>
         {
-            
             const evaluar = document.querySelector('#caracter').value;
             let aprobado = true;
-            
+
             for(const i in palabra_Buscar){
                 if (evaluar == palabra_Buscar[i]){
                     oculto = oculto.guion(i*2, evaluar);
                     aprobado = false;
+                    limpiarCaja();
                 }
                 document.querySelector('#salidaLetra').innerHTML = oculto;
             }
                 if(aprobado){
-                        
+
                         contadorErrores++;
                         document.querySelector('#imagenAhorcado').style.backgroundPosition = -(300*contadorErrores) + 'px 0';
                         console.log(contadorErrores);
+                        limpiarCaja();
                     }
-
         });
-    
     }
-
-
-
 
 
 function mostrar() {
     cargaDePalabras();
     evaluacion();
+    limpiarCaja();
     
 }
 
